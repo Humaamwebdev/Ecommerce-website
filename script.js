d672d8fd991889eda74be3be34de85b63992b5d0
Humaamwebdev <97960596+Humaamwebdev@users.noreply.github.com>
2022-01-18 18:51:54 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,88 @@
+document.querySelector('#login-btn').onclick = () => {
+  document.querySelector('#login-form-content').classList.toggle('active');
+}
+
+document.querySelector('#login-form-close').onclick = () => {
+  document.querySelector('#login-form-content').classList.remove('active');
+}
+
+let menu = document.querySelector('#menu-btn');
+let navbar = document.querySelector('.navigation');
+
+menu.onclick = () =>{
+  menu.classList.toggle('fa-times');
+  navbar.classList.toggle('active');
+}
+
+window.onscroll = () =>{
+
+  menu.classList.remove('fa-times');
+  navbar.classList.remove('active');
+
+  if(window.scrollY > 0){
+    document.querySelector('header').classList.add('active');
+  }else{
+    document.querySelector('header').classList.remove('active');
+  };
+
+}
+
+
+var swiper = new Swiper(".vehicles-slider", {
+  grabCursor: true,
+  centeredSlides: true,  
+  spaceBetween: 20,
+  loop:true,
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable:true,
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
+
+
+
+var swiper = new Swiper(".featured-slider", {
+  grabCursor: true,
+  centeredSlides: true,  
+  spaceBetween: 20,
+  loop:true,
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable:true,
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
+
+
+
+
+
